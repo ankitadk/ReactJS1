24d8026dbe06012d803c9a16e95d6422fc85476b
Ankita_K <ankitakalangutkar17@gmail.com>
2023-10-25 14:34:35 +0530

    episode-2&3

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ReactJS1
 This repo is created to learn about ReactJS Library.
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Catching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,32 +1,56 @@
-/***
- * <div>
- *      <div>
- *          <h1> I'm h1 tag </h1>
- *          <h2> I'm h2 tag </h2>
- *      </div>
- *      <div>
- *          <h1> I'm h1 tag </h1>
- *          <h2> I'm h2 tag </h2>
- *      </div>
- * </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World from React!");
+//React Element
+//React.createElement creates an object and when we render it onto the DOM, it becomes an HTML element
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 // console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm h1 tag"), 
-        React.createElement("h2", {}, "I'm h2 tag")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I'm h1 tag"), 
-        React.createElement("h2", {}, "I'm h2 tag")
-    ])
-]);
-console.log(parent);
+//JSX - is not HTML in JS, it is HTML-like or XML-like syntax
+//JSX (transpiled before it reaches the JS) - PARCEL - Babel
+//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+//React Element
+const heading = (
+  <h1 id="heading" className="head" tabIndex="1">
+    Namaste React using JSX 1
+  </h1>
+);
+console.log(heading);
+
+const elem = <span>Span Tag </span>;
+
+const Title = () => (
+  <h3 id="heading" tabIndex="1">
+    Namaste React using JSX Title 3
+  </h3>
+);
+
+const title = (
+  <h1 id="heading" tabIndex="1">
+    {elem}
+    <Title />
+    Namaste React using JSX 2
+  </h1>
+);
+
+const number = 10000;
+
+//React Functional Component
+//Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h2>{number}</h2>
+    <h2>{100 + 200}</h2>
+    {console.log("Hello React")}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
+root.render(heading);
+root.render(<HeadingComponent />);
